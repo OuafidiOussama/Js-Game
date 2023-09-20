ae8e93b6b4dd3320c96096b1a7b5477f2aecef59
OUAFIDI <o.ouafidi@shellboxes.com>
2023-09-20 22:19:39 +0100

    Animate Character

--- js/main.js
+++ js/main.js
@@ -1,33 +1,58 @@
 const holesNumber = 6;
+let currentHole;
 const grid = document.getElementById('grid');
+function startGame(){
+    for(let i = 0; i < holesNumber; i++){
+        const container = document.createElement('div');
+        container.className = 'container';
+        
+        const blackHole = document.createElement('img');
+        blackHole.src = './assets/balckhole.png';
+        blackHole.alt = '';
+        blackHole.className = 'blackhole rotate';
 
-for(let i = 0; i < holesNumber; i++){
-    const container = document.createElement('div');
-    container.className = 'container';
+        const div = document.createElement('div');
+        div.id = `${i}`;
 
-    const blackhole = document.createElement('img');
-    blackhole.src = './assets/balckhole.png';
-    blackhole.alt = '';
-    blackhole.className = 'blackhole rotate';
-    blackhole.id = `${i+1}`;
+        container.appendChild(blackHole);
+        container.appendChild(div);
+        grid.appendChild(container);
+    }
+
+    setInterval(setCharacter, 2000)
+}
+
+function getRandomHole(){
+    let num = Math.floor(Math.random() * holesNumber);
+    // let lastHole = num;
+    // // if(lastHole){
+    // //     console.log('repeated Hole')
+    // //     getRandomHole();
+    // // }
+    return num.toString();
+}
+
+function setCharacter(){
+
+    const previousCharacter = grid.querySelector('.character');
+    if (previousCharacter) {
+        previousCharacter.remove();
+    }
 
     const character = document.createElement('img');
     character.src = './assets/hungry.png';
     character.alt = '';
     character.className = 'character';
+    character.style.transform = 'scale(0)';
 
-    container.appendChild(blackhole);
-    container.appendChild(character);
-    grid.appendChild(container);
+    let num = getRandomHole();
+    currentHole = document.getElementById(num);
+    
+    currentHole.appendChild(character);
+
+    setTimeout(()=>{
+        character.style.transform = 'scale(1)'
+    }, 1000)
+    
+
 }
-
-function getRandomHole(){
-    let num = Math.floor(Math.random() * holesNumber);
-    let lastHole = num;
-    if(lastHole){
-        console.log('repeated Hole')
-        getRandomHole();
-    }
-    return num;
-}
-
