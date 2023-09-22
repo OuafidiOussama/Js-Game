a43eb77e15e82c1f82ffbbcaf95ed1d04bbcfd1c
OUAFIDI <o.ouafidi@shellboxes.com>
2023-09-22 16:48:19 +0100

    Character States

--- js/main.js
+++ js/main.js
@@ -1,80 +1,82 @@
 const holesNumber = 6;
 let currentHole;
 const grid = document.getElementById('grid');
-function startGame(){
-    for(let i = 0; i < holesNumber; i++){
-        const container = document.createElement('div');
-        container.className = 'container';
-        
-        const blackHole = document.createElement('img');
-        blackHole.src = './assets/balckhole.png';
-        blackHole.alt = '';
-        blackHole.className = 'blackhole rotate';
+let characterStatus = "hungry"; 
+for(let i = 0; i < holesNumber; i++){
+    const container = document.createElement('div');
+    container.className = 'container';
+    
+    const blackHole = document.createElement('img');
+    blackHole.src = './assets/balckhole.png';
+    blackHole.alt = '';
+    blackHole.className = 'blackhole rotate';
 
-        const div = document.createElement('div');
-        div.id = `${i}`;
+    const div = document.createElement('div');
+    div.className = "holder"
+    div.id = `${i}`;
 
-        container.appendChild(blackHole);
-        container.appendChild(div);
-        grid.appendChild(container);
-    }
-
-    requestAnimationFrame(setCharacter)
+    container.appendChild(blackHole);
+    container.appendChild(div);
+    grid.appendChild(container);
 }
 
 function getRandomHole(){
     let num = Math.floor(Math.random() * holesNumber);
-    // let lastHole = num;
-    // // if(lastHole){
-    // //     console.log('repeated Hole')
-    // //     getRandomHole();
-    // // }
+    let lastHole = num;
+    if(lastHole){
+        console.log('repeated Hole')
+        getRandomHole();
+    }
     return num.toString();
 }
 
+
 function setCharacter(){
-
-    const previousCharacter = grid.querySelector('.character');
     
-    if (previousCharacter) {
-        previousCharacter.remove();
-    }
-
-    const character = document.createElement('img');
-    character.src = './assets/hungry.png';
-    character.alt = '';
-    character.className = 'character';
-    character.style.transform = 'scale(0)';
-
-    let num = getRandomHole();
-    currentHole = document.getElementById(num);
-    
-    currentHole.appendChild(character);
-
-    setTimeout(()=>{
+        const character = document.createElement('img');
+        character.src = './assets/hungry.png';
+        character.alt = '';
+        character.className = 'character';
+        character.style.transform = 'scale(0)';
+        characterStatus = "hungry"
+        let num = getRandomHole();
+        currentHole = document.getElementById(num);
+        
+        currentHole.appendChild(character);
         character.style.transform = 'scale(1)';
-        const progress = document.getElementById('progress')
-        let width1 = 0;
-
+        
+        
+        const holder = document.querySelector('.holder img')
+        
+        
         character.addEventListener('click', ()=>{
-            width1 += 4
-            progress.style.width = width1 + "%";
-            
-            setTimeout(() => {
-                character.src ='./assets/success.png';
-            }, 100);
-            character.src = './assets/back.png';
-        });
-        setTimeout(() => {
-            if (character.src.endsWith('hungry.png')) {
-              character.src = './assets/fail.png';
+            const progress = document.getElementById('progress')
+            let width1 = 0;
+            if(characterStatus === "hungry"){
+                width1 += 4
+                progress.style.width = width1 + "%";
+                character.src = "./assets/success.png";
+                characterStatus = "fed";
             }
-          }, 1000);
-    }, 2000)
+        });
+
+        setTimeout(()=>{
+            if(characterStatus === "hungry"){
+                character.src = "./assets/fail.png";
+                characterStatus = "sad"; 
+            }
+        },2000)
+        setTimeout(()=>{
+            character.src = "./assets/back.png";
+        }, 2500);
+        setTimeout(()=>{
+            holder.remove();
+            console.log('deleted');
+        },2800) 
     
     
 }
 
 
-window.onload = startGame();
+window.onload = setInterval(setCharacter, 2000);
 
